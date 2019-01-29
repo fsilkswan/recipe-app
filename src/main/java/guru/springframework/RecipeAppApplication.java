cefb51cf3f0706b5071c6568e60941ef712a1735
fseidenschwan <ferdinand@seidenschwan.de>
2019-01-30 00:34:54 +0100

    Added data transfer objects and converters. Extended RecipeService.

--- src/main/java/guru/springframework/RecipeAppApplication.java
+++ src/main/java/guru/springframework/RecipeAppApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipeAppApplication
 {
+
     public static void main(String[] args)
     {
         SpringApplication.run(RecipeAppApplication.class, args);
     }
+
 }
