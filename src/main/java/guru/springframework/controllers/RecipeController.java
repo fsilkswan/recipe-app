a4da7d78a2bb2edd10a2ae08a038b69f69146399
fseidenschwan <ferdinand@seidenschwan.de>
2019-02-03 00:13:32 +0100

    Added functionality to create new recipes.

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -2,9 +2,12 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guru.springframework.datatransferobjects.RecipeDto;
 import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +21,22 @@ public class RecipeController
     public RecipeController(final RecipeService recipeService)
     {
         this.recipeService = recipeService;
+    }
+
+    @RequestMapping({ "/recipe/new" })
+    public String newRecipe(final Model model)
+    {
+        model.addAttribute("recipeDto", new RecipeDto());
+
+        return "recipe/recipe_form";
+    }
+
+    @PostMapping({ "/recipe/saveOrUpdate" })
+    public String saveOrUpdate(@ModelAttribute final RecipeDto recipeDto)
+    {
+        final RecipeDto savedRecipeDto = recipeService.saveRecipeDto(recipeDto);
+
+        return "redirect:" + "/recipe/show/" + savedRecipeDto.getId();
     }
 
     @RequestMapping({ "/recipe/show/{id}" })
--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -2,13 +2,18 @@ package guru.springframework.controllers;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
 import static org.hamcrest.Matchers.sameInstance;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -20,6 +25,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import guru.springframework.datatransferobjects.RecipeDto;
 import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 
@@ -40,7 +46,25 @@ public final class RecipeControllerTest
     }
 
     @Test
-    public void testMockMvc()
+    public void testProcessRecipeFormPostDataByMockMvc()
+        throws Exception
+    {
+        final RecipeDto recipeDto = new RecipeDto();
+        recipeDto.setId(23L);
+
+        when(recipeServiceMock.saveRecipeDto(any(RecipeDto.class))).thenReturn(recipeDto);
+
+        final MockMvc mockMvc = MockMvcBuilders.standaloneSetup(cut).build();
+        mockMvc.perform(post("/recipe/saveOrUpdate")/* params? */)
+               .andExpect(status().is(302))
+               .andExpect(view().name(is(equalTo("redirect:/recipe/show/23"))))
+               .andExpect(header().string("location", is(equalTo("/recipe/show/23"))));
+
+        verify(recipeServiceMock, times(1)).saveRecipeDto(any(RecipeDto.class));
+    }
+
+    @Test
+    public void testShowRecipeByMockMvc()
         throws Exception
     {
         final Recipe recipe = new Recipe();
@@ -56,5 +80,19 @@ public final class RecipeControllerTest
                .andExpect(model().attribute("recipe", is(sameInstance(recipe))));
 
         verify(recipeServiceMock, times(1)).fetchById(eq(1L));
+    }
+
+    @Test
+    public void testShowRecipeFormByMockMvc()
+        throws Exception
+    {
+        final MockMvc mockMvc = MockMvcBuilders.standaloneSetup(cut).build();
+        mockMvc.perform(get("/recipe/new"))
+               .andExpect(status().isOk())
+               .andExpect(view().name(is(equalTo("recipe/recipe_form"))))
+               .andExpect(model().attributeExists("recipeDto"))
+               .andExpect(model().attribute("recipeDto", isA(RecipeDto.class)));
+
+        verifyZeroInteractions(recipeServiceMock);
     }
 }
