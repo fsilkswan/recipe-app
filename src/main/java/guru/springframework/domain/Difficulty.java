158ad475f784354db6bd5cb44ce1991e8dcb38d1
fseidenschwan <ferdinand@seidenschwan.de>
2019-02-12 20:51:45 +0100

    Update to use ENUM values.

--- src/main/java/guru/springframework/domain/Difficulty.java
+++ src/main/java/guru/springframework/domain/Difficulty.java
@@ -2,5 +2,36 @@ package guru.springframework.domain;
 
 public enum Difficulty
 {
-    EASY, HARD, KIND_OF_HARD, MODERATE;
+    /**
+     *
+     */
+    EASY("Easy"),
+
+    /**
+     *
+     */
+    HARD("Hard"),
+
+    /**
+     *
+     */
+    KIND_OF_HARD("Kind-a-Hard"),
+
+    /**
+     *
+     */
+    MODERATE("Moderate");
+
+    private final String text;
+
+    Difficulty(final String text)
+    {
+        this.text = text;
+    }
+
+    @Override
+    public String toString()
+    {
+        return text;
+    }
 }
