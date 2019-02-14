0b0109ab110560244cf0250e4b3a37f970f84e07
fseidenschwan <ferdinand@seidenschwan.de>
2019-02-15 00:25:11 +0100

    Added unit test.

---
+++ src/test/java/guru/springframework/services/ImageServiceImplTest.java
@@ -0,0 +1,79 @@
+package guru.springframework.services;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+
+/**
+ * Unit tests for class: {@linkplain ImageServiceImpl}
+ *
+ * @author <a href="mailto:ferdinand@seidenschwan.de">Ferdinand Seidenschwan</a>
+ */
+public final class ImageServiceImplTest
+{
+    private ImageServiceImpl cut;
+
+    @Mock
+    private RecipeRepository recipeRepositoryMock;
+
+    /**
+     * Creates the test fixtures.
+     *
+     * @throws Exception
+     *             On errors.
+     */
+    @Before
+    public void beforeEach()
+        throws Exception
+    {
+        MockitoAnnotations.initMocks(this);
+
+        cut = new ImageServiceImpl(recipeRepositoryMock);
+    }
+
+    /**
+     * Test for method: {@linkplain ImageServiceImpl#saveImageFile(Long, org.springframework.web.multipart.MultipartFile)}
+     *
+     * @throws Exception
+     *             On errors.
+     */
+    @Test
+    public void testSaveImageFile()
+        throws Exception
+    {
+        // GIVEN:
+        final Long recipeId = 1L;
+        final MultipartFile multipartFile = new MockMultipartFile("imageFile", "testing.txt", MediaType.TEXT_PLAIN_VALUE, "Spring Framework Guru".getBytes());
+
+        final Recipe recipe = new Recipe();
+        recipe.setId(recipeId);
+        final Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepositoryMock.findById(anyLong())).thenReturn(recipeOptional);
+
+        // WHEN:
+        cut.saveImageFile(recipeId, multipartFile);
+
+        // THEN:
+        verify(recipeRepositoryMock, times(1)).save(same(recipe));
+        assertThat(recipe.getImage().length, is(equalTo(multipartFile.getBytes().length)));
+    }
+}
